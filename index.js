ce5b7a6055b0b9271305b0a800b7db6d2ca9c43c
parikhakshat <68412398+parikhakshat@users.noreply.github.com>
2021-03-15 09:12:33 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-var $hands = $('#staticclock div.hand') // reference all the hand DIVs
+var $hands = $('#liveclock div.hand')
 
-function setclock(fieldval, fieldtype){
- if (fieldtype == 'hrfield'){ //fieldval should be 0-23
-  var hour_as_degree = fieldval / 12 * 360 //express hours in degs
-  $hands.filter('.hour').css({transform: 'rotate(' + hour_as_degree + 'deg)' }) //rotate hour DIV x degrees
- }
- else if (fieldtype == 'minfield'){ //fieldval should be 0-59
-  var minute_as_degree = fieldval / 60 * 360 // same deal in mins
-  $hands.filter('.minute').css({transform: 'rotate(' + minute_as_degree + 'deg)' })
- }
- else if (fieldtype == 'secfield'){ //fieldval should be 0-59
-  var second_as_degree = fieldval /60 * 360
-  $hands.filter('.second').css({transform: 'rotate(' + second_as_degree + 'deg)' })
- }
+window.requestAnimationFrame = window.requestAnimationFrame
+                               || window.mozRequestAnimationFrame
+                               || window.webkitRequestAnimationFrame
+                               || window.msRequestAnimationFrame
+                               || function(f){return setTimeout(f, 1000/60)}
+
+
+function updateclock(){
+ var curdate = new Date()
+ var hour_as_degree = ( curdate.getHours() + curdate.getMinutes()/60 ) / 12 * 360
+ var minute_as_degree = curdate.getMinutes() / 60 * 360
+ var second_as_degree = ( curdate.getSeconds() + curdate.getMilliseconds()/1000 ) /60 * 360
+ $hands.filter('.hour').css({transform: 'rotate(' + hour_as_degree + 'deg)' })
+ $hands.filter('.minute').css({transform: 'rotate(' + minute_as_degree + 'deg)' })
+ $hands.filter('.second').css({transform: 'rotate(' + second_as_degree + 'deg)' })
+ requestAnimationFrame(updateclock) //
 }
+
+requestAnimationFrame(updateclock)
