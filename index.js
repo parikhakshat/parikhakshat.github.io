dec211db76c67098dcabab709cea25d9bba6d97c
parikhakshat <68412398+parikhakshat@users.noreply.github.com>
2021-03-22 21:11:23 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,65 @@
+let departures = ["17:50","12:50","09:50","03:50"]
+
+function playAnimation() {
+	var animationHtml = document.querySelector('.animation');
+	animationHtml.setAttribute('id','active');
+}
+
+function stopAnimation() {
+	var animationHtml = document.querySelector('.animation');
+	animationHtml.removeAttribute('id');
+}
+
+function testFunctionality() {
+	var date = new Date()
+	var full = date.getHours().toString() + ":" + (date.getMinutes() - 3).toString()
+	departures.push(full)
+	console.log(departures);
+}
+
+function removeFunctionality() {
+	departures.pop();
+}
+
+function setTime() {
+var date = new Date()
+var hour = date.getHours()
+var minute = date.getMinutes()
+var seconds = date.getSeconds()
+var hourHtml = document.getElementById("hour")
+var minuteHtml = document.getElementById("minutes")
+var secondHtml = document.getElementById("seconds")
+var prefix = "am"
+var prefixHtml = document.getElementById("prefix")
+if (minute < 10 ) {
+	minute  = "0" + minute.toString()
+}
+
+if (hour > 12) {
+	prefix = "pm"
+}
+hourHtml.innerHTML = hour -12
+minuteHtml.innerHTML = minute
+secondHtml.innerHTML = seconds
+prefixHtml.innerHTML = prefix
+//console.log(date);
+var i;
+for (i = 0; i < departures.length; i++) {
+	var hoursandmin = departures[i].split(":");
+	//console.log(departures[0]);
+	//console.log(departures[1]);
+if (date.getHours()  == parseInt(hoursandmin[0]) && date.getMinutes() > (parseInt(departures[1]) - 10) && date.getMinutes() < parseInt(departures[1])) {
+	playAnimation();
+} else {
+	stopAnimation();
+}
+}
+var t = setTimeout(setTime,1000);
+}
+
+
+
+
+$(document).ready(function(){
+    setTime();
+})
